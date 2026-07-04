a5ce57cb757b20f2ae63ad414314fcb84c523fd0
Shriya Kushte <shriyakushte28@gmail.com>
2026-07-04 19:07:38 +0530

    Fix duplicate axios import in API service

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,24 +1,27 @@
 import axios from "axios";
 
-import axios from "axios";
-
 export const API_BASE_URL =
     import.meta.env.VITE_API_URL || "http://localhost:8080";
 
 const api = axios.create({
-    baseURL: `${API_BASE_URL}/api`,
-    headers: {
-        "Content-Type": "application/json",
-    },
-    withCredentials: true,
+    baseURL: API_BASE_URL,
 });
 
+api.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem("token");
+
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+
+        return config;
+    },
+    (error) => Promise.reject(error)
+);
+
+api.defaults.withCredentials = true;
 api.defaults.xsrfCookieName = "XSRF-TOKEN";
 api.defaults.xsrfHeaderName = "X-XSRF-TOKEN";
-
-export default api;
-
-api.defaults.xsrfCookieName = 'XSRF-TOKEN';
-api.defaults.xsrfHeaderName = 'X-XSRF-TOKEN';
 
 export default api;
